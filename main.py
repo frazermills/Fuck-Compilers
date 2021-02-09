ff51f9236cb2b94650d481570ee1a4ad6abfb5d0
Frazer Mills <77229450+frazermills@users.noreply.github.com>
2021-02-09 15:44:12 +0000

    Added inputs
    I've already committed this but I seem to be having problems with commits not saving

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ DEBUG = True
 
 def main():
     bfcode = str(input("Enter file you want to compiler: "))
+    mode = str(input("Enter mode: "))
+    num_of_inputs = int(input("How many inputs are there? "))
+    inputs = [input("Input next number: ") for i in range(num_of_inputs)]
+
+    if DEBUG: print(bfcode, mode, inputs)
+
     with open(f"{bfcode}.txt", "r") as f:
         code = f.read()
-    
-        comp = Compiler(code)
+
+        comp = Compiler(code, inputs, mode)
         compiled_code = comp.evaluate()
 
 if __name__ == "__main__":
